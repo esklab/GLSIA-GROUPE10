be509dd5459aa68dd0d5ee26d886643a90ee9337
Edouard <ediadjaro710@gmail.com>
2022-04-26 11:51:40 +0000

    first commit avant gestion des utilisateur

---
+++ src/test/java/com/glsiA/projet/ProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glsiA.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
